dfb4a5a59da49ee6bb20c3f48972f59ae7efcf42
Gayanga Kuruppu <50499511+cmdrGuyson@users.noreply.github.com>
2025-09-14 14:50:06 +0530

    feat: add testing environment timezone

--- apps/web/jest.setup.js
+++ apps/web/jest.setup.js
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 
 // Mock environment variables
 process.env.NEXT_PUBLIC_API_URL = "http://localhost:3001";
+process.env.TZ = "UTC";
 
 // Mock window.matchMedia
 Object.defineProperty(window, "matchMedia", {
